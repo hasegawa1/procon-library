bf8b90fbf3cd86104fe1a030c42bc25265664782
hasegawa1 <36944206+hasegawa1@users.noreply.github.com>
2021-06-22 19:01:07 +0900

    Add dijkstra

---
+++ graph/dijkstra.cpp
@@ -0,0 +1,33 @@
+/**
+ * @brief グラフ上の最短経路長（Dijkstra）
+ * @author hasegawa1
+ */
+
+#include <vector>
+#include <queue>
+#include <functional>
+
+template<typename T>
+using WeightedGraph = std::vector<std::vector<std::pair<int,T>>>;
+
+template<typename T>
+std::vector<T> dijkstra(const std::vector<std::vector<std::pair<int,T>>> & adjacency_list, int start, T unreachable = -1) {
+    std::vector<T> dist(adjacency_list.size(), unreachable);
+    dist[start] = 0; // スタートに戻ってくるときはこの行を消す
+    std::priority_queue<std::pair<T,int>, std::vector<std::pair<T,int>>, std::greater<std::pair<T,int>>> pq;
+    pq.emplace(0, start);
+
+    while(!pq.empty()) {
+        const auto [cost, v] = pq.top(); pq.pop();
+        if(dist[v] != unreachable && dist[v] < cost) continue;
+        for(const auto [u, cost2]: adjacency_list[v]) {
+            if(cost2 == unreachable) continue;
+            T next_cost = cost + cost2;
+            if(dist[u] != unreachable && dist[u] <= next_cost) continue;
+            dist[u] = next_cost;
+            pq.emplace(next_cost, u);
+        }
+    }
+
+    return dist;
+}
---
+++ test/aoj_grl_1_a.test.cpp
@@ -0,0 +1,32 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A"
+
+#include <iostream>
+#include "../graph/dijkstra.cpp"
+
+using namespace std;
+
+template<typename T>
+using WeightedGraph = std::vector<std::vector<std::pair<int,T>>>;
+
+int main(void) {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int V, E, r;
+    cin >> V >> E >> r;
+
+    WeightedGraph<int64_t> g(V);
+    for(int i=0; i<E; i++) {
+        int s, t, d;
+        cin >> s >> t >> d;
+        g[s].emplace_back(t, d);
+    }
+
+    for(const auto d: dijkstra(g, r)) {
+        if(d == -1) {
+            cout << "INF" << endl;
+        } else {
+            cout << d << endl;
+        }
+    }
+}
