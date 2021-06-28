e0d4c385448e278015fe6ac3aaab5b854c8e6275
hasegawa1 <36944206+hasegawa1@users.noreply.github.com>
2021-06-28 16:45:07 +0900

    Improve CumulativeSum.cpp

--- other/CumulativeSum.cpp
+++ other/CumulativeSum.cpp
@@ -13,12 +13,12 @@ private:
     const int _n;
     std::vector<T> _cumulative_sum;
 public:
-    explicit CumulativeSum(std::vector<T> v): _n(v.size()) {
-        v.emplace_back(0);
-        _cumulative_sum.reserve(_n);
-        std::exclusive_scan(v.begin(), v.end(), std::back_inserter(_cumulative_sum), T());
+    explicit CumulativeSum(std::vector<T> v): _n(v.size()), _cumulative_sum(v) {
+        _cumulative_sum.emplace_back(T());
+        std::exclusive_scan(_cumulative_sum.begin(), _cumulative_sum.end(), _cumulative_sum.begin(), T());
     }
 
+    // [l, r)
     T operator()(int l, int r) {
         assert(0 <= l && r <= _n);
         return _cumulative_sum[r] - _cumulative_sum[l];
