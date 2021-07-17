c6c0410dc78011865095b3901dc32144fd5122eb
hasegawa1 <36944206+hasegawa1@users.noreply.github.com>
2021-07-17 13:23:02 +0900

    Add diameter.cpp

---
+++ aizu/grl_5_a.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/all/GRL_5_A"
+
+#include <iostream>
+#include "../graph/breadth_first_search.cpp"
+#include "../graph/dijkstra.cpp"
+#include "../graph/diameter.cpp"
+
+using namespace std;
+
+int main(void) {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    WeightedGraph<int> g(n);
+    for(int i=0; i<n-1; i++) {
+        int s, t, w;
+        cin >> s >> t >> w;
+        g[s].emplace_back(t, w);
+        g[t].emplace_back(s, w);
+    }
+
+    cout << diameter(g) << endl;
+}
---
+++ graph/diameter.cpp
@@ -0,0 +1,25 @@
+/**
+ * @brief 木の直径
+ * @author hasegawa1
+ */
+
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+using WeightedGraph = std::vector<std::vector<std::pair<int,T>>>;
+
+int diameter(const std::vector<std::vector<int>> &g) {
+    auto dist = bfs(g, 0);
+    int v = std::distance(dist.begin(), std::max_element(dist.begin(), dist.end()));
+    dist = bfs(g, v);
+    return *std::max_element(dist.begin(), dist.end());
+}
+
+template<typename T>
+int diameter(const std::vector<std::vector<std::pair<int,T>>> &g) {
+    auto dist = dijkstra(g, 0);
+    int v = std::distance(dist.begin(), std::max_element(dist.begin(), dist.end()));
+    dist = dijkstra(g, v);
+    return *std::max_element(dist.begin(), dist.end());
+}
