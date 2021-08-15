bc46c4d0d3bc1c953bbb1b6613d4cf97d512ffb1
hasegawa1 <36944206+hasegawa1@users.noreply.github.com>
2021-08-15 11:47:44 +0900

    Add next_array.cpp

---
+++ string/next_array.cpp
@@ -0,0 +1,19 @@
+/**
+ * @brief next_array
+ * @author hasegawa1
+ */
+
+#include <vector>
+#include <string>
+
+std::vector<std::vector<int>> next_array(const std::string &s) {
+    int n = s.size();
+    std::vector<std::vector<int>> res(n+1, std::vector<int>(26, n));
+    for (int i=n-1; i>=0; i--) {
+        for (int j=0; j<26; j++) {
+            res[i][j] = res[i+1][j];
+        }
+        res[i][s[i]-'a'] = i;
+    }
+    return res;
+}
