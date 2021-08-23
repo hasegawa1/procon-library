fa3fb034a88ec113200f33f95695c9832f84edac
hasegawa1 <36944206+hasegawa1@users.noreply.github.com>
2021-08-23 17:03:50 +0900

    Improve zeta/moebius_transform.cpp

--- convolution/zeta_transform.cpp
+++ convolution/zeta_transform.cpp
@@ -8,8 +8,8 @@
 
 template<typename T>
 std::vector<T> zeta_transform(std::vector<T> v, bool lower) {
-    int n = v.size();
-    assert((n == 0) || ((n & (n-1)) == 0)); //ispow2
+    const int n = v.size();
+    assert((n & (n-1)) == 0); //ispow2
 
     if(lower) {
         for(int i=1; i<n; i<<=1) {
